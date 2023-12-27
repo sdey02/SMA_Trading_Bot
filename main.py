ce3402c201a95d4f83173c1eb852640bdfcdabb7
Shreyans <78007563+sdey02@users.noreply.github.com>
2023-12-27 16:32:23 -0700

    Currenty creates a polars df that stores ticker, price, and the 3 trends. Working on calcuating the trend.

--- main.py
+++ main.py
@@ -8,9 +8,54 @@ stocks = pl.read_csv("sp_500_stocks.csv")
 final_dataframe = pl.DataFrame(
     {
         "Ticker": [],
-        "30 day Trend": [],
+        "Buy or Sell?": [], 
+        "Current Price": [],
+        "20 Day Trend": [],
+        "50 Day Trend": [],
+        "200 Day Trend": [],
         "Current Trend": [],
-        "Market Capitalization": [], 
-        "Number of Shares to Buy": [],
     }
 )
+
+trend_dataframe = pl.DataFrame(
+    {
+        "Ticker": [],
+        "Uptrend Counter": [],
+        "Downtrend Counter": [],
+    }
+)
+
+final_dataframe = final_dataframe.with_columns(
+    [
+        pl.col("Ticker").cast(pl.Utf8),
+        pl.col("Buy or Sell?").cast(pl.Utf8),
+        pl.col("Current Price").cast(pl.Float64),
+        pl.col("20 Day Trend").cast(pl.Float64),
+        pl.col("50 Day Trend").cast(pl.Float64),
+        pl.col("200 Day Trend").cast(pl.Float64),
+        pl.col("Current Trend").cast(pl.Utf8),
+    ]
+)
+
+for stock in stocks['Ticker'][:5]:
+    twenty_day_sma = yahoo_fin.stock_info.get_data(stock, interval='1d')['close'][-20:].mean()
+    fifty_day_sma = yahoo_fin.stock_info.get_data(stock, interval='1d')['close'][-50:].mean()
+    two_hundred_day_sma = yahoo_fin.stock_info.get_data(stock, interval='1d')['close'][-200:].mean()
+    price = yahoo_fin.stock_info.get_live_price(stock)
+
+    stock_info = pl.DataFrame(
+        {
+            "Ticker": [stock],
+            "Buy or Sell?": ['N/A'], 
+            "Current Price": [price],
+            "20 Day Trend": [twenty_day_sma],
+            "50 Day Trend": [fifty_day_sma],
+            "200 Day Trend": [two_hundred_day_sma],
+            "Current Trend": ['N/A'],
+        }
+    )
+
+    final_dataframe = pl.concat([final_dataframe, stock_info])
+
+
+print(final_dataframe)
